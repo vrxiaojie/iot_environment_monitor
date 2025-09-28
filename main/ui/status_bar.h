11f882035788001703d8dca2a850b24bf7dc8a28
VRxiaojie <1814014516@qq.com>
2025-09-28 09:06:06 +0800

    add: 状态栏隐藏/显示API

--- main/ui/status_bar.h
+++ main/ui/status_bar.h
@@ -6,3 +6,5 @@ void status_bar_set_time(const char *time_str);
 void status_bar_set_wifi_state(bool connected);
 void status_bar_set_battery_level(float level);
 void status_bar_set_charge_state(bool charging);
+void status_bar_show();
+void status_bar_hide();
