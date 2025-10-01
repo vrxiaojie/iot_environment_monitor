e68c0cb7ba9efe95c353d04514c2e61a6e6d3496
VRxiaojie <1814014516@qq.com>
2025-10-01 10:34:25 +0800

    add: nvs存储MQTT相关设置

--- components/nvs_helper/include/nvs_helper.h
+++ components/nvs_helper/include/nvs_helper.h
@@ -12,6 +12,20 @@ typedef struct {
 
 extern power_settings_t power_settings;
 
+typedef struct
+{
+    const char *uri;
+    uint32_t port;
+    const char *username;
+    const char *password;
+    uint8_t upload_interval;
+    uint8_t auto_conn;
+} mqtt_user_config_t;
+
+extern mqtt_user_config_t mqtt_user_config;
+
 void nvs_init(void);
 void nvs_read_power_settings(void);
 void nvs_write_power_settings(power_settings_t new_power_settings);
+void mqtt_read_settings(void);
+void mqtt_write_settings(mqtt_user_config_t new_mqtt_user_config);
